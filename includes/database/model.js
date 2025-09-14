dc9a24a700a224f6049bea19910aa18f2df71d9c
bonifacioancheta10-afk <bonifacioancheta10@gmail.com>
2025-09-15 07:54:35 +0800

    Update model.js

--- includes/database/model.js
+++ includes/database/model.js
@@ -1,19 +1,24 @@
 module.exports = function (input) {
 	const force = false;
 
+	// Import ng lahat ng models
 	const Users = require("./models/users")(input);
 	const Threads = require("./models/threads")(input);
 	const Currencies = require("./models/currencies")(input);
+	const Ban = require("./models/ban")(input); // ✅ bagong ban model
 
+	// Sync lahat ng tables (auto create kung wala pa)
 	Users.sync({ force });
 	Threads.sync({ force });
 	Currencies.sync({ force });
+	Ban.sync({ force });
 
 	return {
 		model: {
 			Users,
 			Threads,
-			Currencies
+			Currencies,
+			Ban
 		},
 		use: function (modelName) {
 			return this.model[`${modelName}`];
