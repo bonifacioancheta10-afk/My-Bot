51857a776b713fb81c72b165d360ae1def46c645
bonifacioancheta10-afk <bonifacioancheta10@gmail.com>
2025-09-15 13:14:13 +0800

    Update threads.js

--- includes/database/models/threads.js
+++ includes/database/models/threads.js
@@ -1,21 +1,24 @@
-module.exports = function({ sequelize, Sequelize }) {
-	let Threads = sequelize.define('Threads', {
-		num: {
-			type: Sequelize.INTEGER,
-			primaryKey: true,
-			autoIncrement: true
-		},
-		threadID: {
-			type: Sequelize.BIGINT,
-			unique: true
-		},
-        threadInfo: {
-            type: Sequelize.JSON
-        },
-		data: {
-			type: Sequelize.JSON
-		}
-	});
+// models/threads.js
+const { DataTypes } = require("sequelize");
 
-	return Threads;
-}
+module.exports = (sequelize) => {
+  const Threads = sequelize.define("Threads", {
+    num: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    threadID: {
+      type: DataTypes.BIGINT,
+      unique: true,
+    },
+    threadInfo: {
+      type: DataTypes.JSON,
+    },
+    data: {
+      type: DataTypes.JSON,
+    },
+  });
+
+  return Threads;
+};
