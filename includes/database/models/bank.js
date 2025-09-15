7a2231ed4914c0bca19dbedd1d86863388ad8aa2
bonifacioancheta10-afk <bonifacioancheta10@gmail.com>
2025-09-15 12:40:50 +0800

    Update bank.js

--- includes/database/models/bank.js
+++ includes/database/models/bank.js
@@ -1,18 +1,15 @@
-// database/models/bank.js
-module.exports = (sequelize) => {
-  const { DataTypes } = require("sequelize");
-
+module.exports = (sequelize, DataTypes) => {
   const Bank = sequelize.define("Bank", {
     userID: {
       type: DataTypes.STRING,
       primaryKey: true,
-      allowNull: false
+      allowNull: false,
     },
     balance: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      defaultValue: 0
-    }
+      defaultValue: 0,
+    },
   });
 
   return Bank;
