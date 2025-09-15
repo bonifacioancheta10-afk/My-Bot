45a81269af3111de315738f180f7143f06cbaeeb
bonifacioancheta10-afk <bonifacioancheta10@gmail.com>
2025-09-15 08:16:43 +0800

    Update model.js

--- includes/database/model.js
+++ includes/database/model.js
@@ -4,15 +4,17 @@ module.exports = function (input) {
   const Users = require("./models/users")(input);
   const Threads = require("./models/threads")(input);
   const Currencies = require("./models/currencies")(input);
+  const Bans = require("./models/ban")(input);
   const Bank = require("./models/bank")(input);
-  const Ban = require("./models/ban")(input);
-  const Check = require("./models/check")(input);   // ✅ Added
+  const Shop = require("./models/shop")(input);
+  const Check = require("./models/check")(input);   // ✅ Check model
 
   Users.sync({ force });
   Threads.sync({ force });
   Currencies.sync({ force });
+  Bans.sync({ force });
   Bank.sync({ force });
-  Ban.sync({ force });
+  Shop.sync({ force });
   Check.sync({ force });
 
   return {
@@ -20,12 +22,13 @@ module.exports = function (input) {
       Users,
       Threads,
       Currencies,
+      Bans,
       Bank,
-      Ban,
-      Check
+      Shop,
+      Check,   // ✅ expose Check
     },
     use: function (modelName) {
       return this.model[`${modelName}`];
-    }
-  }
-}
+    },
+  };
+};
