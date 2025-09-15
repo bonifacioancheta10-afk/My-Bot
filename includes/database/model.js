62731cfa72f9aa0f6682fff28f146678aa535231
bonifacioancheta10-afk <bonifacioancheta10@gmail.com>
2025-09-15 08:32:53 +0800

    Update model.js

--- includes/database/model.js
+++ includes/database/model.js
@@ -8,7 +8,8 @@ module.exports = function (input) {
   const Bank = require("./models/bank")(input);
   const Shop = require("./models/shop")(input);
   const Check = require("./models/check")(input);
-  const LockGroup = require("./models/lockGroup")(input); // ✅
+  const LockGroup = require("./models/lockGroup")(input);
+  const Rules = require("./models/rules")(input); // ✅
 
   Users.sync({ force });
   Threads.sync({ force });
@@ -17,7 +18,8 @@ module.exports = function (input) {
   Bank.sync({ force });
   Shop.sync({ force });
   Check.sync({ force });
-  LockGroup.sync({ force }); // ✅
+  LockGroup.sync({ force });
+  Rules.sync({ force }); // ✅
 
   return {
     model: {
@@ -28,7 +30,8 @@ module.exports = function (input) {
       Bank,
       Shop,
       Check,
-      LockGroup, // ✅
+      LockGroup,
+      Rules, // ✅
     },
     use: function (modelName) {
       return this.model[`${modelName}`];
