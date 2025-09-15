7fa1937a99e48db8ea6f0e2c01b79f7bcb01b1c3
bonifacioancheta10-afk <bonifacioancheta10@gmail.com>
2025-09-15 13:05:31 +0800

    Update currencies.js

--- includes/database/models/currencies.js
+++ includes/database/models/currencies.js
@@ -1,5 +1,7 @@
 // database/models/currencies.js
-module.exports = (sequelize, DataTypes) => {
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
   const Currencies = sequelize.define("Currencies", {
     num: {
       type: DataTypes.INTEGER,
